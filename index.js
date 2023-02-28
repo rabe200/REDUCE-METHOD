7103443b122be22d54455f8de67acae46a720668
rabewtf <rabenklar@gmx.de>
2023-02-28 23:31:53 +0100

    add start of exercise

---
+++ index.js
@@ -0,0 +1,53 @@
+//utils
+const cut = () => {
+    console.log("<------");
+    console.log(":::::::");
+    console.log("------>");}
+// :::1:::
+//sum of flat array
+console.clear();
+arr = [1, 1, 1, 1, 1]
+console.log("initial array: ", arr)
+
+const sum = arr.reduce((acc, currentValue) => {
+return acc + currentValue}, 0);
+
+console.log(":::1::: reduced array: ", sum);
+cut();
+// :::2:::
+// sum of object (key:pairs)
+const obj = [{x: 1}, {x:2 }, {x:3}, {x:4}];
+console.log("initial object: ", obj)
+const sum2 = obj.reduce((acc, currentValue) => {
+    return acc + currentValue.x}, 0);
+
+console.log(":::2::: reduced object: ", sum2)
+cut()
+// :::3:::
+// flatten an array of arrays
+
+const flattened = [
+[0, 1],
+[2, 3],
+[4, 5],
+].reduce((acc, currentValue) =>
+acc.concat(currentValue)
+)
+
+console.log(":::3::: reduced and concatenated: ", flattened)
+cut()
+// :::4::: 
+//more steps / used to arrow function
+const toFlatten = [[0,1],[2,3],[4,5]]
+console.log("toFlatten: ", toFlatten)
+
+const flatArr = toFlatten.reduce((acc, currentValue) => {
+    return acc.concat(currentValue)
+});
+console.log("flatArr: ", flatArr)
+
+const flatArrSum = flatArr.reduce((acc, currentValue) => {
+    return acc + currentValue;
+}, 0)
+console.log(":::4::: sum of flatArr: ", flatArrSum)
+cut()
